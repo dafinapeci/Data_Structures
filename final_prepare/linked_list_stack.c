1c4d9a65290e66546d187b0c081b1c61ebb56699
Dafina Peci <106171859+dafinapeci@users.noreply.github.com>
2024-12-28 22:10:04 +0300

    Create linked_list_stack.c

---
+++ final_prepare/linked_list_stack.c
@@ -0,0 +1,58 @@
+// Linked List using Stack
+
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct Node {
+	int data;
+	struct Node* link;
+};
+
+struct Node* top = NULL;
+
+void Push(int data) {
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+
+	temp->data = data;
+	temp->link = top;
+
+	top = temp;
+}
+
+void Pop() {
+	struct Node* temp = top;
+	if (top == NULL) {
+		return; // empty stack
+	}
+	top = temp->link;
+	free(temp);
+	
+}
+
+void Print() {
+	struct Node* temp = top;
+	while (temp->link != NULL) {
+		printf("Data: %d\tLink: %d\n", temp->data, temp->link);
+		temp = temp->link;
+	}
+	printf("\nlast item link: %d, and data:%d\n", -1, temp->data);
+}
+
+int main() {
+	
+	Push(5);
+	Push(15);
+	Push(2);
+	Push(20);
+
+	printf("before deletion\n");
+	Print();
+
+	Pop();
+	printf("after deletion\n");
+	Print();
+}
